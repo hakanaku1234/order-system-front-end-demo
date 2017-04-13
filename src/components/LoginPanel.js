ce54ee54fa14691901dd86ca2b7b6e00a52468ca
zhangxq <878773559@qq.com>
2017-04-13 16:58:29 +0800

    表单 手机号码校验

--- src/components/LoginPanel.js
+++ src/components/LoginPanel.js
@@ -19,15 +19,21 @@ export default class LoginPanel extends React.Component {
     super(...args);
 
     this.state = {
+      isPhoneErr: false,
       phone: '',
       vcode: ''
     };
 
     this._login = this._login.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this._isPhoneErr = this._isPhoneErr.bind(this);
   }
 
   _login() {
+    if(this.state.isPhoneErr) {
+      return false;
+    }
+
     let onSuccess = this.props.onSuccess;
     let prefix = window.__CONFIG__.default.api.prefix;
     myFetch.post(prefix + '/user', {
@@ -40,11 +46,27 @@ export default class LoginPanel extends React.Component {
       })
   }
 
+  _isPhoneErr(phone) {
+    if(phone && /^1[3|4|5|8|][0-9]\d{8}$/.test(phone)) {
+      this.setState({
+        isPhoneErr: false
+      })
+    } else {
+      this.setState({
+        isPhoneErr: '手机号码格式不正确.'
+      })
+    }
+  }
+
   handleChange(event) {
     let name = event.target.getAttribute('name');
     let _obj = {};
     _obj[name] = event.target.value;
     this.setState(_obj);
+
+    if (name == 'phone') {
+      this._isPhoneErr(event.target.value);
+    }
 
     event.stopPropagation();
     event.preventDefault();
@@ -60,6 +82,7 @@ export default class LoginPanel extends React.Component {
             onChange={this.handleChange}
             value={this.state.phone}
             style={{width: '100%'}}
+            errorText={this.state.isPhoneErr}
             hintText="请输入手机号码"/>
           <TextField
             name="vcode"
